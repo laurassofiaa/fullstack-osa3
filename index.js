ec5f9a8d123e404c7fdcc5d2f1613112326413e4
laura <laura.s.toivanen@aalto.fi>
2023-01-23 12:57:12 +0200

    teht. 3.15-3.18

--- index.js
+++ index.js
@@ -14,43 +14,9 @@ app.use(express.static('build'))
 //morgan.token('body', (req, res) => JSON.stringify({name: req.body.name, number: req.body.number}))
 //app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// let persons = [
-//     {
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523",
-//         id: 1
-//     },
-//     {
-//         name: "Dan Abramov",
-//         number: "12-43-234345",
-//         id: 2
-//     },
-//     {
-//         name: "Mary Poppendieck",
-//         number: "39-23-6423122",
-//         id: 3
-//     },
-//     {
-//         name: "Arto Hellas",
-//         number: "111-111-1111",
-//         id: 4
-//     }
-// ]
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-
-// const generateId = () => {
-//   const maxId = persons.length > 0
-//     ? Math.max(...persons.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -59,38 +25,31 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'content missing' })
   }
 
-  // if (!body.name || !body.number ){
-  //   return response.status(400).json({ 
-  //     error: 'Content missing' 
-  //   })
-  // }
-
-  //const exists = Person.find(body.name)
-  //const names = persons.map(per => per.name)
-  //const exists = names.find(name => name === body.name)
-  //console.log(exists)
-
-  // if (exists != undefined){
-  //   return response.status(400).json({ 
-  //     error: 'Name already added' 
-  //   })
-  // } 
-
   const person = new Person ({
     name: body.name,
     number: body.number,
   })
-
-  //persons = persons.concat(person)
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
 })
 
-// app.get('/api/persons', (req, res) => {
-//   res.json(persons)
-// })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  //console.log(person)
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -98,25 +57,49 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 }) 
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info of ${persons.length} people</p> 
+  Person.find({}).then(persons => {
+    res.send(`<p> Phonebook has info of ${persons.length} people </p> 
     <p>${new Date} </p>`)
+  })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+// testaa vielä virheidenkäsittelyt ennen committia!!
